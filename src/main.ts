53e31834c3366d72b766d1c786c3be0dca82872f
Kenneth Sumang <kennethsumang@outlook.com>
2021-11-20 09:47:03 +0800

    feat(views): add support for hbs

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import * as session from 'express-session';
 import * as csurf from 'csurf';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get<ConfigService>(ConfigService);
 
   // for CSRF protection
@@ -18,6 +20,12 @@ async function bootstrap() {
 
   // for CORS
   // app.enableCors();
+
+  // for views
+  // app.useStaticAssets(join(__dirname, '..', 'public'));
+  // app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  // app.setViewEngine('hbs');
+
   await app.listen(3000);
 }
 bootstrap();
